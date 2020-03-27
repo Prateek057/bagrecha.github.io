d8fedcf37de5e0315499be3d69e5286ebac2c108
PrateekSB <bagrecha@mediamarktsaturn.com>
2020-03-27 18:38:51 +0100

    Updates

---
+++ precache-manifest.107204e4b53034e17e302182794a59bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275c411a9bf74b2c0a71f10f49ec3e43",
+    "url": "/bagrecha.github.io/index.html"
+  },
+  {
+    "revision": "b58e3180fcaf4667b2f1",
+    "url": "/bagrecha.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e75080deef35b62e7a67",
+    "url": "/bagrecha.github.io/static/js/2.d5610c69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bagrecha.github.io/static/js/2.d5610c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b58e3180fcaf4667b2f1",
+    "url": "/bagrecha.github.io/static/js/main.d826ee89.chunk.js"
+  },
+  {
+    "revision": "63af487adb8e18d5dd50",
+    "url": "/bagrecha.github.io/static/js/runtime-main.89deb79a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bagrecha.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c880e450b01e4c07cbae2875e9614a15.js"
+  "/bagrecha.github.io/precache-manifest.107204e4b53034e17e302182794a59bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bagrecha.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
